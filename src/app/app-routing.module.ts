2da689ce85794141c9017e8c32329183c145ad5a
Saravanan S <saravanans.design@gmail.com>
2022-11-26 21:45:57 +0530

    Login & Registration Page UI

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,32 @@
+import { HomeComponent } from './home/home.component';
+import { ForgetPasswordComponent } from './forget-password/forget-password.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { LoginComponent } from './login/login.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    "path" : "",
+    "component" : LoginComponent
+  },
+  {
+    "path" : "login",
+    "component" : LoginComponent
+  },
+  {
+    "path" : "home",
+    "component" : HomeComponent
+  },
+  {
+    "path" : "registration",
+    "component" : RegistrationComponent
+  },
+  {
+    "path" : "forget-password",
+    "component" : ForgetPasswordComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { ForgetPasswordComponent } from './forget-password/forget-password.component';
+import { HomeComponent } from './home/home.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeComponent,
+    LoginComponent,
+    RegistrationComponent,
+    ForgetPasswordComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
